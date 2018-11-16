7f6a4f4faeb5ed7dab9c5793861bbc6d2b72eaa7
netacademia <netacademia@msi>
2018-11-17 00:40:40 +0100

    Array,cycles

--- cli/src/main/java/com/hoe/cli/Application.java
+++ cli/src/main/java/com/hoe/cli/Application.java
@@ -17,9 +17,20 @@ public class Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Hero hero = new Hero("PapaSmurf", ".....");
-        System.out.println(hero.getName());
-        System.out.println(hero.getDescription());
+
+        Hero[] heroes = new Hero[4];
+                
+        
+        heroes[0] = new Hero("PapaSmurf", ".....");
+        heroes[1] = new Hero("BrainySmurf", ".....");
+        heroes[2] = new Hero("HeftySmurf", ".....");
+        heroes[3] = new Hero("Smurfette", ".....");
+
+        byte i=0;
+        do {            
+            System.out.println(heroes[i].getName());            
+            i++;         
+        } while (i<heroes.length);
     }
     
 }
